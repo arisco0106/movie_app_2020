662374a181b14cefefbd090f9453ffa302036f7d
heo <arisco@naver.com>
2020-11-08 17:48:43 +0900

    Updates

---
+++ precache-manifest.db3e76528d8f91fd562b45ca890aa4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b4b9fdd48d4c559c262416113d9cc0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e404a55f13dc44351aa8",
+    "url": "/movie_app_2020/static/css/main.8358de6d.chunk.css"
+  },
+  {
+    "revision": "edadb73d0a80228485be",
+    "url": "/movie_app_2020/static/js/2.145cee8d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app_2020/static/js/2.145cee8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e404a55f13dc44351aa8",
+    "url": "/movie_app_2020/static/js/main.c0c95b98.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
